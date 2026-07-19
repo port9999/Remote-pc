de63e7d04d6656d2b17cdbb05c485dd3da1130c8
port9999 <code009999999@gmail.com>
2026-07-19 11:24:48 +0530

    Update exp.c with exp2

--- exp.c
+++ exp.c
@@ -119,3 +119,121 @@ int main() {
  } 
  return 0; 
 }
+<_________________________________________>
+#include <stdio.h>
+#include <stdlib.h>
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+struct Node
+{
+int e;
+Position previous;
+Position next;
+};
+void Insert(int x, List l, Position p)
+{
+Position TmpCell;
+TmpCell = (struct Node *) malloc(sizeof(struct Node));
+if (TmpCell == NULL)
+printf("Memory out of space\n");
+else
+{
+TmpCell->e = x;
+TmpCell->previous = p;
+TmpCell->next = p->next;
+if (p->next != NULL)
+p->next->previous = TmpCell;
+p->next = TmpCell;
+}
+}
+int isLast(Position p)
+{
+return (p->next == NULL);
+}
+Position Find(int x, List l)
+{
+Position p = l->next;
+while (p != NULL && p->e != x)
+p = p->next;
+return p;
+}
+void Delete(int x, List l)
+{
+Position p, p1, p2;
+p = Find(x, l);
+if (p != NULL)
+{
+p1 = p->previous;
+p2 = p->next;
+p1->next = p->next;
+if (p2 != NULL) /* if the node is not the last
+node */
+p2->previous = p->previous;
+free(p);
+}
+else
+printf("Element does not exist!!!\n");
+}
+void Display(List l)
+{
+Position p = l->next;
+printf("The list elements are :: ");
+while (p != NULL)
+{
+printf("%d -> ", p->e);
+p = p->next;
+}
+printf("\n");
+}
+int main()
+{
+int x, pos, ch, i;
+List l;
+List p;
+l = (struct Node *) malloc(sizeof(struct Node));
+l->previous = NULL;
+l->next = NULL;
+printf("DOUBLY LINKED LIST IMPLEMENTATION OF LIST ADT\n\n");
+do
+{
+printf("\n\n1. INSERT\t 2. DELETE\t 3. FIND\t 4. PRINT\t 5.
+QUIT\n\n");
+printf("Enter the choice :: ");
+scanf("%d", &ch);
+switch (ch)
+{
+case 1:
+p = l;
+printf("Enter the element to be inserted :: ");
+scanf("%d", &x);
+printf("Enter the position of the element :: ");
+scanf("%d", &pos);
+for (i = 1; i < pos; i++)
+p = p->next;
+Insert(x, l, p);
+break;
+case 2:
+printf("Enter the element to be deleted :: ");
+scanf("%d", &x);
+Delete(x, l);
+break;
+case 3:
+printf("Enter the element to be searched :: ");
+scanf("%d", &x);
+p = Find(x, l);
+if (p == NULL)
+printf("Element does not exist!!!\n");
+else
+printf("Element exists!!!\n");
+break;
+case 4:
+Display(l);
+break;
+default:
+break;
+}
+} while (ch < 5);
+return 0;
+}
