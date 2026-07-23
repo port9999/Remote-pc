f143932856b3162ef435520688ac944263f89964
port9999 <code009999999@gmail.com>
2026-07-23 09:17:50 +0530

    Update exp.java

--- exp.java
+++ exp.java
@@ -31,3 +31,124 @@ System.out.println("Employee
 address:"+address);
 }
 }
+class Programmer extends Employee
+{
+float bp,salary,hra,da,swf,pf;
+public Programmer()
+{
+System.out.println("Enter Bas Pay:");
+bp=get.nextFloat();
+hra=bp*10/100;
+da=bp*97/100;
+pf=bp*12/100;
+swf=bp*1/10000;
+salary=bp+hra+da-pf-s
+wf;
+}
+void display()
+{
+System.out.println("Programmer
+Details:"); super.display();
+System.out.println("Basic
+Pay:"+bp);
+System.out.println("HRA:"+hra);
+System.out.println("DA:"+da);
+System.out.println("Staff welare amount:"+swf);
+System.out.println("P F:"+pf);
+System.out.println("Salary:"+salary);
+}
+}
+class AssistProf extends Employee
+{
+float bp,salary,hra,da,swf,pf;
+public AssistProf()
+{
+System.out.println("Enter Bas Pay:");
+bp=get.nextFloat();
+hra=bp*10/100;
+da=bp*97/100;
+pf=bp*12/100;
+swf=bp
+*1/10000;
+salary=bp+hra+da-pf-swf;
+}
+void display()
+{
+System.out.println("Assstant Profesor Details:");
+super.display();
+System.out.println("Basic Pay:"+bp);
+System.out.println("HRA:"+hra); System.out.println("DA:"+da);
+System.out.println("Staff welare amount:"+swf);
+System.out.println("P F:"+pf);
+System.out.println("Salary:"+salary);
+}
+}
+class AssoProf extends Employee
+{
+float
+bp,salary,hra,da,swf,pf;
+public AssoProf() {
+System.out.println("Enter Bas Pay:");
+bp=get.nextFloat();
+hra=bp*10/100;
+da=bp*97/100;
+pf=bp*12/100;
+swf=bp
+*1/10000;
+salary=bp+hra+da-pf-swf;
+}
+void display()
+{
+System.out.println("Associate Professor Details:");
+super.display();
+System.out.println("Basic Pay:"+bp);
+System.out.println("HRA:"+hra); System.out.println("DA:"+da);
+System.out.println("Staff welare amount:"+swf);
+System.out.println("P F:"+pf);
+System.out.println("Salary:"+salary);
+}
+}
+class Professor extends Employee
+{
+float
+bp,salary,hra,da,swf,pf;
+public Professor()
+{
+System.out.println("Enter Bas Pay:");
+bp=get.nextFloat();
+hra=bp*10/100;
+da=bp*97/100;
+pf=bp*12/100;
+swf=bp
+*1/10000;
+salary=bp+hra+da-pf-swf;
+}
+void display()
+{
+System.out.println("Professor
+Details:"); super.display();
+System.out.println("Basic Pay:"+bp);
+System.out.println("HRA:"+hra);
+System.out.println("DA:"+da);
+System.out.println("Staff welare amount:"+swf);
+System.out.println("P F:"+pf);
+System.out.println("Salary:"+salary);
+}
+}
+public class Employees
+{
+public static void main(String[] args)
+{
+System.out.println("Enter Employee Details:"); Employee e=new
+Employee();
+e.display();
+Programmer obj1=new Programmer();
+obj1.display();
+AssistProf obj2=new
+AssistProf(); obj2.display();
+AssoProf obj3=new AssoProf();
+obj3.display();
+Professor obj4=new Professor();
+obj4.display();
+}
+}
